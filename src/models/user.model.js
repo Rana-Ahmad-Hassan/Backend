452aa1a76e727f52d34000ed256abe27f6587f59
IAM-Rana-Ahmad <r80228927@gmail.com>
2024-01-20 03:33:36 +0500

    generating the jwt tokens and encrypt the userSchema password using the bcrypt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     userName: {
@@ -43,6 +45,44 @@ const userSchema = new Schema({
         type: String,
     },
 }, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = ()=>{
+    return jwt.sign(
+        {
+            _id:this._id,
+            username: this.userName,
+            email: this.email,
+            fullName: this.fullName
+
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIEY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = ()=>{
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 const User = mongoose.model("User", userSchema);
