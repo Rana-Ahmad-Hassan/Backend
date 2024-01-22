64a841302e6c8086b2c4f6722f5a69aec8e54f97
IAM-Rana-Ahmad <r80228927@gmail.com>
2024-01-23 00:17:27 +0500

    adding some authentication in the controller of user

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,49 @@
+import User from "../models/user.model.js";
+import ApiError from "../utils/apiError.js";
 import AsyncHandler from "../utils/asyncHandler.js";
+import fileUpload from "../utils/cloudinary.js";
 
-const registerUser= AsyncHandler(async(req,res)=>{
+const registerUser = AsyncHandler(async (req, res) => {
     res.status(200).json({
-        message:"ok"
+        message: "ok"
     })
-     const {email}= req.body;
-     console.log(email)
-    
+    const { userName, email, fullName, password } = req.body;
+
+
+    if (
+        [userName, email, fullName,  password].some((field) => field?.trim() === "")
+    ) {
+        throw new ApiError(400, "All the fields are require")
+    }
+
+
+    const existedUser = User.findOne(
+        {
+            $or: [{ email }, { userName }]
+        }
+    )
+
+    if(existedUser){
+        throw new ApiError(409,"User is already existed")
+    }
+
+    const avatarLocalPath=req.files?.avatar[0].path;
+    const coverImgLocalPath=req.files?.coverImage[0].path;
+
+
+    if(!avatarLocalPath){
+        throw new ApiError(404,"Avatar image is required")
+    }
+
+    const avatar= await fileUpload(avatarLocalPath);
+    const coverImage= await fileUpload(coverImgLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400,"Avatar image file is required")
+    }
+
+
 })
 
 
-export {registerUser}
+export { registerUser }
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import {  registerUser } from "../controllers/user.controller.js";
+import { uploadFIle } from "../middlewares/multer.middleware.js";
 
 const router=Router()
 
-router.route("/reg").post(registerUser)
+router.route("/reg").post(
+    uploadFIle.fields(
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
+    ),
+    registerUser)
 
 
 export default router;
