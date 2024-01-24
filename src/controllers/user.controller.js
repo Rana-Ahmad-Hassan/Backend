7c725384707c3ab444962f10b26dde7699b48058
IAM-Rana-Ahmad <r80228927@gmail.com>
2024-01-24 23:51:24 +0500

    adding the login controller i the user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,9 +19,9 @@ const registerUser = AsyncHandler(async (req, res) => {
         throw new ApiError(409, "User is already existed");
     }
 
-    const avatarLocalPath = req.files?.image[0].path;
-    const coverImgLocalPath = req.files?.coverImage[0].path;
-    
+    const avatarLocalPath = req.files?.image[0]?.path;
+    const coverImgLocalPath = req.files?.coverImage[0]?.path;
+
     if (!avatarLocalPath) {
         throw new ApiError(404, "Avatar image is required");
     }
@@ -29,7 +29,7 @@ const registerUser = AsyncHandler(async (req, res) => {
 
     const avatar = await cloudUpload(`${avatarLocalPath}`);
     const coverImage = await cloudUpload(`${coverImgLocalPath}`);
-    
+
 
     if (!avatar) {
         throw new ApiError(400, "Error uploading avatar image");
@@ -56,5 +56,67 @@ const registerUser = AsyncHandler(async (req, res) => {
         new ApiResponse(200, createdUser, "User registered successfully")
     );
 });
+
+
+const generateAccessAndRefreshToken = async (userId) => {
+    try {
+        const user = await User.findById(userId)
+        const accessToken = user.generateAccessToken()
+        const refreshToken = user.generateRefreshToken()
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false })
+
+        return { accessToken, refreshToken }
+    } catch (error) {
+        throw new ApiError(500, "something went wrong while generating the access and refresh token")
+    }
+}
+
+
+
+
+const logInUser = AsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    if (!email) {
+        throw new ApiError(400, "Email is required")
+    }
+
+    const user = User.findOne({ email })
+    if (!user) {
+        throw new ApiError(404, "User is not registered")
+    }
+
+    const validPassword = await user.isPasswordCorrect(password)
+
+    if (!validPassword) {
+        throw new ApiError(404, "User is not registered")
+    }
+
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id)
+
+    const loggedInUser = await User.findOne(user._id).select("-password -refreshToken")
+
+    const options = {
+        httpOnly: true,
+        secure: true
+    }
+
+    return res.status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: logInUser, accessToken, refreshToken
+                },
+                "user Logged in successfully"
+            )
+        )
+
+
+})
 
 export { registerUser };
